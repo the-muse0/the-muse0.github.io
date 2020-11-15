12a87778fcba0558e91ec0dff1ab088e99f773ca
the-muse0 <74465288+the-muse0@users.noreply.github.com>
2020-11-14 20:07:11 -0600

    onclick
    trying to onclick and move image

--- hl_races.js
+++ hl_races.js
@@ -1,2 +1,27 @@
 "use strict";
 
+/*
+Final project CIS 370
+ going to the races
+ * Date - 11/14/20
+ * @author Helen Lee
+ */
+ 
+ 
+ 
+ const circles = 
+ document.querySelectorAll('.circle');
+ let activeLight = 0;
+ 
+ setInterval(changeLight, onclick);
+ 
+ function changeLight(){
+ circles[activeLight].className = 'circle';
+ activeLight++;
+ 
+	
+	
+currentLight.classList.add(currentLight.getAttribute('color'));
+
+	
+ }
