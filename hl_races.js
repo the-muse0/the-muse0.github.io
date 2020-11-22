faa91d942e770d45a309da5bc7aee6d1cb047718
the-muse0 <74465288+the-muse0@users.noreply.github.com>
2020-11-22 13:08:54 -0600

    correcting
    redo code

--- hl_races.js
+++ hl_races.js
@@ -13,12 +13,18 @@ Final project CIS 370
  document.querySelectorAll('.circle');
  let activeLight = 0;
  
- setInterval(changeLight, onclick);
+ setInterval(changeLight, 1000);
  
  function changeLight(){
  circles[activeLight].className = 'circle';
  activeLight++;
  
+	if(activeLight >2) {
+		activeLight = 0;
+	}
+	
+  const currentLight = circles[activeLight];
+	
 	
 	
 currentLight.classList.add(currentLight.getAttribute('color'));
